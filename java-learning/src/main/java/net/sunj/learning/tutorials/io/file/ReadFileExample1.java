4f7c1debd845c909ef8e0c185617024af463e322
sunj <jjeom87@gmail.com>
2018-02-06 10:25:59 +0900

    update

---
+++ java-learning/src/main/java/net/sunj/learning/tutorials/io/file/ReadFileExample1.java
@@ -0,0 +1,43 @@
+package net.sunj.learning.tutorials.io.file;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileExample1 {
+
+  private static final String FILENAME =
+      ReadFileExample1.class.getClassLoader().getResource("files/filename.txt").getPath();
+
+  public static void main(String[] args) {
+
+    BufferedReader br = null;
+    FileReader fr = null;
+
+    try {
+      // br = new BufferedReader(new FileReader(FILENAME));
+      fr = new FileReader(FILENAME);
+      br = new BufferedReader(fr);
+
+      String sCurrentLine;
+
+      while ((sCurrentLine = br.readLine()) != null) {
+        System.out.println(sCurrentLine);
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+
+    } finally {
+      try {
+        if (br != null)
+          br.close();
+
+        if (fr != null)
+          fr.close();
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+  }
+}
