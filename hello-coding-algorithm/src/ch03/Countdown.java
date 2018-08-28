db86e8f539f8ce593a95512f961caab451631f89
jjeom87 <jjeom87@gmail.com>
2018-08-28 13:07:15 +0900

    Fix up

---
+++ hello-coding-algorithm/src/ch03/Countdown.java
@@ -0,0 +1,18 @@
+package ch03;
+public class Countdown {
+
+    private static void countdown(int i) {
+        System.out.println(i);
+
+        // base case
+        if (i <= 0) {
+            return;
+        } else {
+            countdown(i - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        countdown(5);
+    }
+}
---
+++ hello-coding-algorithm/src/ch03/Factorial.java
@@ -0,0 +1,15 @@
+package ch03;
+public class Factorial {
+
+    private static int fact(int x) {
+        if (x == 1) {
+            return 1;
+        } else {
+            return x * fact(x - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+}
